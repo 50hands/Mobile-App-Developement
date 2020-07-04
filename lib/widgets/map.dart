ccc3d31241d1794265b2863d26242e3fb19ad600
gaurav <gpathak161999@gmail.com>
2020-07-04 16:31:57 +0530

    Color Theme and size reduction

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -3,30 +3,25 @@ import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
-
-
 class Map extends StatefulWidget {
   @override
   _MapState createState() => _MapState();
 }
 
 class _MapState extends State<Map> {
-    var clat,clong;
+  var clat, clong;
 
-
-   void _getCurrentLocation()  async {
-    final  position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    if (this.mounted){
-    setState(() {
-      // _location="${position.latitude},${position.longitude}";
-      clat=position.latitude;
-      clong=position.longitude;
-
-    });
+  void _getCurrentLocation() async {
+    final position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    if (this.mounted) {
+      setState(() {
+        // _location="${position.latitude},${position.longitude}";
+        clat = position.latitude;
+        clong = position.longitude;
+      });
     }
-    
   }
-  
 
   @override
   void initState() {
@@ -37,52 +32,54 @@ class _MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body:clat!=null?
-    FlutterMap(
-    options: new MapOptions(
-      center: new LatLng(clat,clong),
-      zoom: 15.0,
-      maxZoom: 18,
-    ),
-    layers: [
-      new TileLayerOptions(
-        urlTemplate: "https://api.mapbox.com/styles/v1/shravanpv201/ckbyuchqs1wil1imnkj1e7s58/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2hyYXZhbnB2MjAxIiwiYSI6ImNrYmM1Y2tiZjAxMzEyeG5ycTM1cDFrbXgifQ.rui0pxbaTT_gTD9_uGTHAA",
-        additionalOptions: {
-          'accessToken': 'pk.eyJ1Ijoic2hyYXZhbnB2MjAxIiwiYSI6ImNrYmM1Y2tiZjAxMzEyeG5ycTM1cDFrbXgifQ.rui0pxbaTT_gTD9_uGTHAA>',
-          'id': 'shravanpv201.ckbbv3mfl237n22o9dlpuijsd-4aw95',
-        },
-      ),
-      new MarkerLayerOptions(
-        markers: [
-          new Marker(
-            width: 80.0,
-            height: 80.0,
-            point: new LatLng(clat, clong),
-            builder: (ctx) =>
-            new Container(
-              child: Icon(Icons.location_on,color: Colors.blue,)
+      body: clat != null
+          ? FlutterMap(
+              options: new MapOptions(
+                center: new LatLng(clat, clong),
+                zoom: 15.0,
+                maxZoom: 18,
+              ),
+              layers: [
+                new TileLayerOptions(
+                  urlTemplate:
+                      "https://api.mapbox.com/styles/v1/shravanpv201/ckbyuchqs1wil1imnkj1e7s58/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2hyYXZhbnB2MjAxIiwiYSI6ImNrYmM1Y2tiZjAxMzEyeG5ycTM1cDFrbXgifQ.rui0pxbaTT_gTD9_uGTHAA",
+                  additionalOptions: {
+                    'accessToken':
+                        'pk.eyJ1Ijoic2hyYXZhbnB2MjAxIiwiYSI6ImNrYmM1Y2tiZjAxMzEyeG5ycTM1cDFrbXgifQ.rui0pxbaTT_gTD9_uGTHAA>',
+                    'id': 'shravanpv201.ckbbv3mfl237n22o9dlpuijsd-4aw95',
+                  },
+                ),
+                new MarkerLayerOptions(
+                  markers: [
+                    new Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: new LatLng(clat, clong),
+                      builder: (ctx) => new Container(
+                          child: Icon(
+                        Icons.location_on,
+                        color: Colors.blue,
+                      )),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Patience is Key for Success - 50-Hands Team'),
+                  ],
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    ],
-  )
-                          :
-                          Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                CircularProgressIndicator(),
-                                SizedBox(height: 15,),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Patience is Key for Success - 50-Hands Team'),
-                                  ],
-                                ),
-                              ],
-                            ),
-    
     );
-    }
+  }
 }
-
